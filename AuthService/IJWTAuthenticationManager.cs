a9b3f78825cae64933647d6855d3e955161b49af
Burak AKSU <96burakaksu@gmail.com>
2022-12-13 14:44:19 +0300

    Add project files.

---
+++ AuthService/IJWTAuthenticationManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService
+{
+    public interface IJWTAuthenticationManager
+    {
+        string Authenticate(string key, string pass);
+    }
+}
